c92907fd49cf964448679c8b4852b7a538e71adb
Jer <Jer@DESKTOP-8T3N04Q>
2019-04-08 18:09:04 -0600

    Se agrega la clase de cliente en el BLL

---
+++ Let's_Go_PL/BLL_LetsGO/Catalogos_Mantenimientos/Cls_TBL_CLIENTE_BLL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_LetsGO.Catalogos_Mantenimientos
+{
+    public class Cls_TBL_CLIENTE_BLL
+    {
+
+
+    }
+}
