e03658bd299f9cfca3ad567bf30a70bd298192ab
alexriveraelizondo <alexriv9418@hotmail.com>
2019-04-10 23:49:29 -0600

    listar

--- Let's_Go_PL/BLL_LetsGO/Catalogos_Mantenimientos/Cls_TBL_CLIENTE_BLL.cs
+++ Let's_Go_PL/BLL_LetsGO/Catalogos_Mantenimientos/Cls_TBL_CLIENTE_BLL.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BLL_LetsGO.svc_Cat_y_Mat;
+//using BLL_LetsGO.svc_Cat_y_Mat;
 
 namespace BLL_LetsGO.Catalogos_Mantenimientos
 {
     public class Cls_TBL_CLIENTE_BLL
     {
 
-        LetsGo_InterfaceClient client = new LetsGo_InterfaceClient();
+        //LetsGo_InterfaceClient client = new LetsGo_InterfaceClient();
 
-        svc_Cat_y_Mat.ListarDatosRequest
+        //svc_Cat_y_Mat.ListarDatosRequest
 
 
     }
