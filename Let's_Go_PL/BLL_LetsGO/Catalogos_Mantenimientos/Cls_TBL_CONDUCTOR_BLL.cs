c441ccc020413dbfe4b0523a53125c736c32d63b
emism27 <45403150+emism27@users.noreply.github.com>
2019-04-14 22:16:13 -0600

    Método Listar_Conductor

--- Let's_Go_PL/BLL_LetsGO/Catalogos_Mantenimientos/Cls_TBL_CONDUCTOR_BLL.cs
+++ Let's_Go_PL/BLL_LetsGO/Catalogos_Mantenimientos/Cls_TBL_CONDUCTOR_BLL.cs
@@ -3,12 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL_LetsGO.Catalogos_Mantenimientos;
+using System.Data;
 
 namespace BLL_LetsGO.Catalogos_Mantenimientos
 {
-    public class Cls_TBL_CONDUCTOR_BLL
+    class Cls_TBL_CONDUCTOR_BLL
     {
-        
 
+        public void Listar_Conductor(ref Cls_TBL_CONDUCTOR_DAL Obj_CONDUCTOR_DAL)
+        {
+            //Variable de mensaje de error
+
+            string sMsjerror = string.Empty;
+
+            try
+            {
+
+                //Conexión al servicio
+                svc_Cat_Mat.LetsGo_InterfaceClient Obj_svc_LetsGo = new svc_Cat_Mat.LetsGo_InterfaceClient();
+
+                string sNombreSP = "[SCH_CUENTA].[sp_select_TBL_CONDUCTOR]";
+
+                //Se crea DataTable para ser llenado
+                DataTable dt = new DataTable();
+
+
+                //Se llena el DataTable
+                dt = Obj_svc_LetsGo.ListarDatos(sNombreSP, ref sMsjerror);
+
+                if (sMsjerror == string.Empty)
+                {
+                    Obj_CONDUCTOR_DAL.Obj_DT = dt;
+                    sMsjerror = string.Empty;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                sMsjerror = ex.Message.ToString();
+            }
+        }
     }
 }
