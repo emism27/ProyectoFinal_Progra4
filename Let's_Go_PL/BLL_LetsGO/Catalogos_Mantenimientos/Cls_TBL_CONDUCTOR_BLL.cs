3daca7f879ee7faecf32adf114b22258dd208d3a
emism27 <45403150+emism27@users.noreply.github.com>
2019-04-16 18:20:52 -0600

    se borran librerias innecesarias

--- Let's_Go_PL/BLL_LetsGO/Catalogos_Mantenimientos/Cls_TBL_CONDUCTOR_BLL.cs
+++ Let's_Go_PL/BLL_LetsGO/Catalogos_Mantenimientos/Cls_TBL_CONDUCTOR_BLL.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DAL_LetsGO.Catalogos_Mantenimientos;
 using System.Data;
 
